f3b2f7509548ecea88b24a216fcbadfe66d76f1d
SatanshuMishra <satanshumishra@outlook.com>
2023-07-24 21:55:51 -0700

    Build 2.1.2 | 🖥️ Compiler Error Fixes

--- next-personal-portfolio/personal-portfolio/app/components/animationComponents/Reveal.tsx
+++ next-personal-portfolio/personal-portfolio/app/components/animationComponents/Reveal.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect, useRef } from "react";
 import { motion, useAnimation } from "framer-motion";
 import { useInView } from "react-intersection-observer";
